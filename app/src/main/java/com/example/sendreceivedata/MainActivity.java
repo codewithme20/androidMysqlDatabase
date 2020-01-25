190cda50718b1178a762cb924a76b0fa091a0b27
shuman20 <shumanbhandari20@gmail.com>
2020-01-25 19:46:19 +0545

    first commit

---
+++ app/src/main/java/com/example/sendreceivedata/MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.sendreceivedata;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText username,email;
+    private Button save,viewData;
+    private String sendUrl="https://codewithme20.000webhostapp.com/test/getData.php";
+    private RequestQueue requestQueue;
+    private  static  final  String TAG=MainActivity.class.getSimpleName();
+    int sucess;
+    private String TAG_SUCESS="sucess";
+    private String TAG_MESSAGE="message";
+    private String tag_json_obj="json_obj_req";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username=findViewById(R.id.txtUsername);
+        email=findViewById(R.id.txtEmail);
+        save=findViewById(R.id.btnSave);
+        viewData=findViewById(R.id.btnViewData);
+        requestQueue=Volley.newRequestQueue(getApplicationContext());
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendData();
+            }
+        });
+
+
+
+    }
+
+    private  void sendData(){
+        StringRequest request=new StringRequest(Request.Method.POST, sendUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jobj = new JSONObject(response);
+                    sucess = jobj.getInt(TAG_SUCESS);
+                    if (sucess == 1) {
+                        Toast.makeText(MainActivity.this, jobj.getString(TAG_MESSAGE), Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, jobj.getString(TAG_MESSAGE), Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(MainActivity.this, "Error Occured", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, error.getMessage().toString(), Toast.LENGTH_SHORT).show();
+            }
+        }){
+            public Map<String,String> getParams(){
+                Map<String, String> params=new HashMap<String, String>();
+                params.put("username",username.getText().toString());
+                params.put("email",email.getText().toString());
+                return params;
+            }
+        };
+        request.setRetryPolicy(new DefaultRetryPolicy(10000,1,1.0f));
+        requestQueue.add(request);
+
+    }
+
+}
